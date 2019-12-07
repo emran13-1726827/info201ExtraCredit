6ee6c4fc3a94b34de44bd39a51b650ac304e67b9
emran13-1726827 <49257499+emran13-1726827@users.noreply.github.com>
2019-12-06 18:13:25 -0800

    Add files via upload

---
+++ server.R
@@ -0,0 +1,28 @@
+#
+# This is the server logic of a Shiny web application. You can run the
+# application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(dplyr)
+
+# Define server logic required to draw a histogram
+shinyServer(function(input, output) {
+    selectedData <- reactive({
+        data = read.csv("datatest.csv", stringsAsFactors = FALSE)
+        
+        #iris[, c(input$xcol, input$ycol)]
+    })
+    output$distPlot <- renderPlot({
+        d = data[[input$val]]
+        hist(as.numeric(d))
+        # data %>% select(Temperature) %>% nth(1) %>% hist
+        
+
+    })
+
+})
---
+++ ui.R
@@ -0,0 +1,78 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+
+# Define UI for application that draws a histogram
+shinyUI(fluidPage(
+    theme = shinytheme("cerulean"),
+    navbarPage(
+        "Ozone Layer Level Detection",
+        #This will create a page
+        tabPanel(
+            "Introduction",
+            titlePanel("Ozone Level Detection Data Set"),
+            br(),
+            h5("This time series describes the ground ozone concentration throughout 6 years."),
+            h5("The goal of this observation is to 'predict' whether it is an 'ozone day' or not."),
+            br(),
+            h5("Ozone day - means when weather conditions are very likely to mix with pollutions
+               and form a high level of ozone near the ground and that can cause harmful health effects."),
+            br(),
+            h5("This data was collected from 1998 to 2004 at the Houston, Galveston and Brazoria area."),
+            h5("The data was collected to bring awareness and educate people about pollusion issues and how the tempreture, humity and CO2 levels are affected."),
+            br(),
+            h5("Source:"),
+            h5("Kun Zhang, zhang.kun05 '@' gmail.com, Department of Computer Science, Xavier University of Lousiana"),
+            h5("Wei Fan, wei.fan '@' gmail.com, IBM T.J.Watson Research"),
+            h5("XiaoJing Yuan, xyuan '@' uh.edu, Engineering Technology Department, College of Technology, University of Houston"),
+            sidebarPanel(
+                selectInput('val', 'Table Value', choices = c( "Temperature", "Humidity", "Light",  "CO2", "HumidityRatio"), selected = "Temperature")
+                
+                
+            ),
+            mainPanel(
+                plotOutput('distPlot')
+            )
+            
+        ),
+        #Title for the page
+        tabPanel(
+            "Discussion",
+            
+            #This will output the title for the page
+            titlePanel(
+                "Discussion on the time series data"),
+            br(),
+            h4("what stakeholders might be impacted by the time series?"),
+            br(),
+            h5("From the time series data, we can see that we are all stakeholders as long as we are living on this planet."),
+            h5("People and businesses should take action to reduce emissions of ozone-causing pollutants because at the end of the"),
+            h5("day we are ruining our planet and we will be the one that are affected by this because we won't have clean"),
+            br(),
+            h4("How might it be used to do good in the world?"),
+            br(),
+            h5("This information can be used to spread awareness to individuals and bussinesses so we can make a change in this world."),
+            h5("This can also help motivate people in participating in 'Earth Day' and other activities like planting more trees to ger a cleaner air."),
+            br(),
+            h4("What, in short, do you conclude from your analysis? "),
+            br(),
+            h5("In conclusion, "),
+        ),
+        tabPanel(
+            "Reflection and Code",
+            titlePanel(
+                "Reflection on what I learned from the time series data"),
+            br(),
+            h5("From this time series data, I learned that---- What questions do I have"),
+            br(),
+            h5("Code - Appendix"),
+        )
+        )))
+            
